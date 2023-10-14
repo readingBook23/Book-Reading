fcdb00f06f840fe180be1e86dcfde1e1da809d32
osamaraed53 <osamaraed53@gmail.com>
2023-10-14 14:50:54 +0300

    html and CSS may be done just need some reviwe

--- js/bookCategories.js
+++ js/bookCategories.js
@@ -1,12 +1,12 @@
 function openNav() {
   const openBtn = document.querySelector('.openbtn');
   const onRValue = openBtn.getAttribute('on-r');
-  if (onRValue == "true"){
-  openBtn.setAttribute('on-r', 'false');
+  if (onRValue == "false"){
+  openBtn.setAttribute('on-r', 'true');
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
 }else {
-  openBtn.setAttribute('on-r', 'true');
+  openBtn.setAttribute('on-r', 'false');
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
 }
@@ -14,4 +14,9 @@ function openNav() {
 
 }
 
-// function closeNav()
+function closeNav() {
+  const openBtn = document.querySelector('.openbtn');
+  openBtn.setAttribute('on-r', 'false');
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
